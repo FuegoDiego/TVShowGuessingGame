db6ea68fd619601c25c348220d2d342ecfbcae2d
Diego Chavez <diego.chavez27@d155.org>
2026-02-20 07:45:24 -0600

    Initial Commit

---
+++ TVShowGuessingGame/TVShowGuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVShowGuessingGameApp.swift
+//  TVShowGuessingGame
+//
+//  Created by DIEGO CHAVEZ on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct TVShowGuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
