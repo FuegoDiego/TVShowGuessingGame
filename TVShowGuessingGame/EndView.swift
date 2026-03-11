2b21b5e4c13945708dd4df68b6b3b0e1a19aa0f2
jacksong <jacksongerambia@yahoo.com>
2026-03-11 07:37:07 -0500

    Update EndView layout and navigation structure
    Refactor EndView to use NavigationStack and improve UI elements.

--- TVShowGuessingGame/EndView.swift
+++ TVShowGuessingGame/EndView.swift
@@ -8,22 +8,51 @@
 import SwiftUI
 
 struct EndView: View {
-    @Environment(\.dismiss) var dismiss
-    @Binding var path: NavigationPath
+    
+    
     var body: some View {
-        VStack {
-            Text("Game Over")
-                .font(.title)
-            Text("Score:")
-            
-            Button("Home"){
+        NavigationStack {
+            VStack {
+                Text("Game Over")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                Text("Score:")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
                 
+                NavigationLink(destination: ContentView()) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.blue)
+                            .frame(width: 150, height: 50)
+                        
+                        Text("Home")
+                            .foregroundStyle(.white)
+                            .font(.title)
+                        
+                    }
+                }
+                
+                NavigationLink(destination: LeaderboardView()) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.blue)
+                            .frame(width: 200, height: 50)
+                        
+                        Text("Leaderboard")
+                            .foregroundStyle(.white)
+                            .font(.title)
+                        
+                    }
+                }
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.black)
         }
         
     }
 }
 
 #Preview {
-    EndView(path: .constant(NavigationPath()))
+    EndView()
 }
